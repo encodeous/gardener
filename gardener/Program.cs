dcbc58b3a097b091bb582db9765760b486614757
encodeous <encodeous7@gmail.com>
2020-06-17 17:15:09 -0400

    Removed Compile Error

--- gardener/Program.cs
+++ gardener/Program.cs
@@ -27,7 +27,6 @@ namespace gardener
 
         public async Task MainAsync()
         {
-            Compile Error Test
             if (await GithubChecker.UpdateAvailable())
             {
                 Console.WriteLine("An update is available. The bot will update shortly");
