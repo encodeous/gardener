8285d5180d2ef7720e145745d24e829f6418bf00
encodeous <encodeous7@gmail.com>
2020-06-22 12:35:14 -0400

    less sensitive

--- gardener/Filtering/ChatFilter.cs
+++ gardener/Filtering/ChatFilter.cs
@@ -18,41 +18,40 @@ namespace gardener.Filtering
                 Network = ToxiNet.GetToxiNet();
             }
             var token = new CancellationTokenSource(200);
-            Task.Run(() =>
+            Task.Run(async () =>
             {
                 if (message.Channel.Id == 724633073693622362)
                 {
                     var result = Network.Predict(message.Content);
-                    if (result[0].Prediction < 0.6)
+                    if (result[0].Prediction < 0.3)
                     {
-                        message.Channel.SendMessageAsync(embed: GetEmbed(result));
+                        if (result[0].Prediction < 0.1)
+                        {
+                            await message.DeleteAsync();
+                        }
+                        await message.Channel.SendMessageAsync(embed: GetEmbed(result, result[0].Prediction < 0.1));
                     }
                 }
             }, token.Token);
         }
 
-        public static Embed GetEmbed(ToxiNetResult[] result)
+        public static Embed GetEmbed(ToxiNetResult[] result, bool deleted)
         {
             var footer = new EmbedFooterBuilder()
             {
                 Text = "Powered by https://github.com/encodeous/toxinet"
             };
             StringBuilder sb = new StringBuilder();
-            sb.Append("```\n");
             Array.Sort(result, (o, o1) => o1.Prediction.CompareTo(o.Prediction));
 
             foreach (var pred in result)
             {
-                if (pred.Prediction > 0.1)
-                {
-                    sb.Append($"[ {pred.PredictionType} {(pred.Prediction * 100.0)} % ]\n");
-                }
+                sb.Append($"`{pred.PredictionType} {(pred.Prediction * 100.0):0.0}% `");
             }
-            sb.Append("```\n");
             return new EmbedBuilder()
             {
                 Color = Color.Blue,
-                Title = "**You have been Flagged by ToxiNet**",
+                Title = deleted? "*Your message has been deleted**" : "**Your message was determined to be offensive**",
                 Description = sb.ToString(),
                 Footer = footer
             }.Build();
