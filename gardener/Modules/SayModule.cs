85332c6b282d62877a74d392800775124f111e91
encodeous <encodeous7@gmail.com>
2020-07-02 11:19:14 -0400

    say command

---
+++ gardener/Modules/SayModule.cs
@@ -0,0 +1,18 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gardener.Modules
+{
+    public class SayModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("say")]
+        [RequireOwner]
+        public Task Say(string text)
+        {
+            return Context.Channel.SendMessageAsync(text);
+        }
+    }
+}
